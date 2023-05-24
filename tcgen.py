85dc5da15f89ccb6fcdd0ebe31b41911ee5dafb1
Kaua Junio <kauajuno@gmail.com>
2023-05-24 19:44:16 -0300

    [OpheliaGL] Cleanup

--- tcgen.py
+++ tcgen.py
@@ -1,7 +1,6 @@
 import os
 import random
 
-gn_prefixes = ["The ", "A ", ""]
 gn_adjectives = ["Black ", "White ", "Feral ", "Brutal ", "Unholy ", "Holy ", "Legendary ", "Calm ", "Furious ", "Brave ", "Majestic ", "Gloomy ", "Vibrant ", "Whispering ", "Enchanted ", "Mysterious ", "Radiant ", "Savage ", "Tranquil ", "Glistening ", "Eternal ", "Magical ", "Serene ", "Spectacular ", "Fearless ", "Glorious ", "Wild ", "Misty ", "Silent ", "Fierce ", "Dazzling ", "Ancient ", "Awe-inspiring ", "Grim ", "Enigmatic ", "Harmonious ", "Resplendent ", "Boundless ", "Alluring ", "Untamed ", "Noble ", "Supreme ", "Peaceful ", "Raging ", "Celestial ", "Imposing ", "Breathtaking ", "Solemn ", "Vivid ", "Fathomless ", "Unforgiving ", "Surreal ", "Majestic ",
                  "Everlasting ", "Epic ", "Stormy ", "Untouched ", "Roaring ", "Vast ", "Untamed ", "Magnificent ", "Fiery ", "Gentle ", "Unyielding ", "Captivating ", "Distant ", "Ethereal ", "Harsh ", "Spellbinding ", "Mythical ", "Immaculate ", "Transcendent ", "Boundless ", "Endless ", "Untrodden ", "Mesmerizing ", "Enchanting ", "Primeval ", "Mighty ", "Dynamic ", "Peerless ", "Grand ", "Mystical ", "Thundering ", "Stunning ", "Rugged ", "Cascading ", "Infinite ", "Luminous ", "Evolving ", "Unblemished ", "Solitary ", "Roaming ", "Serenading ", "Uncharted ", "Stellar ", "Enthralling ", "Renowned ", "Unspoiled ", "Picturesque ", "Radiant ", "Undying ", "Rapturous ", "Astonishing ", "Epic "]
 gn_names = ["Sword ", "Soldier ", "Eagle ", "Tribe ", "Legion ",
@@ -26,7 +25,6 @@ for i in range(1, total_testcases + 1):
     with open(filename, 'w') as file:
         for _ in range(inputs):
             genres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-            prefix = random.choice(gn_prefixes)
             adjective = random.choice(gn_adjectives)
             name = random.choice(gn_names)
             connector = random.choice(gn_connectors)
@@ -47,7 +45,7 @@ for i in range(1, total_testcases + 1):
             genres_string = ' '.join(str(num) for num in selectedGenres)
 
             file.write("1\n")
-            file.write(prefix + adjective + name + connector + finals + "\n")
+            file.write(adjective + name + connector + finals + "\n")
             file.write(str(downloads) + "\n")
             file.write(genres_string + "\n")
             file.write(str(review) + "\n\n")
